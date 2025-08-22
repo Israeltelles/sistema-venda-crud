c75a1db5cf3efe0fdf420abee976e898e7af24d3
Israel <83608691+Israeltelles@users.noreply.github.com>
2025-08-22 20:57:07 -0300

    Add files via upload

---
+++ Site-vendas/config.php
@@ -0,0 +1,14 @@
+<?php
+$servidor = "localhost";
+$usuario = "root";
+$senha = "";
+$banco = "vendas"; 
+$conexao = new mysqli($servidor, $usuario, $senha, $banco);
+
+//if ($conn->connect_error) {
+//    die("Falha na conexão: " . $conn->connect_error);
+//}
+//else{
+//    echo 'Conexão realizada com sucesso';
+//}
+?>
